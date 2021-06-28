74e7d9d37949da40ce008794d056adc0ddd36036
username <1151555861@qq.com>
2021-06-28 17:45:03 +0800

    first commit

---
+++ chapter2.2-9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace chapter2._2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> noValue = new Nullable<int>();
+            object noValueBoxed = noValue;
+            Console.WriteLine(noValueBoxed == null);
+
+            Nullable<int> someValue = new Nullable<int>(5);
+            object someValueBoxed = someValue;
+            Console.WriteLine(someValueBoxed.GetType());
+            Console.ReadKey();
+        }
+    }
+}
+
